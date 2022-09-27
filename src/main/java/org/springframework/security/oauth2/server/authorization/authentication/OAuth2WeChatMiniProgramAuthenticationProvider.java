60be6560692ab83bf6f917e787835df053f94c8d
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-09-27 17:04:04 +0800

    :bug: 修正验证提示语

--- src/main/java/org/springframework/security/oauth2/server/authorization/authentication/OAuth2WeChatMiniProgramAuthenticationProvider.java
+++ src/main/java/org/springframework/security/oauth2/server/authorization/authentication/OAuth2WeChatMiniProgramAuthenticationProvider.java
@@ -72,7 +72,7 @@ public class OAuth2WeChatMiniProgramAuthenticationProvider implements Authentica
 	private WeChatMiniProgramService wechatMiniProgramService;
 
 	public OAuth2WeChatMiniProgramAuthenticationProvider(HttpSecurity builder) {
-		Assert.notNull(builder, "HttpSecurityBuilder<?> 不能为空");
+		Assert.notNull(builder, "HttpSecurity 不能为空");
 		this.builder = builder;
 		builder.authenticationProvider(this);
 	}
