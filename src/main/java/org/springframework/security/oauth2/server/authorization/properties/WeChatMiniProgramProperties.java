b94ddf0d17e0d805999512e9797d799807a3a033
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-12-27 11:06:31 +0800

    :sparkles: 新增默认AppID

--- src/main/java/org/springframework/security/oauth2/server/authorization/properties/WeChatMiniProgramProperties.java
+++ src/main/java/org/springframework/security/oauth2/server/authorization/properties/WeChatMiniProgramProperties.java
@@ -9,9 +9,9 @@ package org.springframework.security.oauth2.server.authorization.properties;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,10 +20,13 @@ package org.springframework.security.oauth2.server.authorization.properties;
  * #L%
  */
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.security.oauth2.server.authorization.client.WeChatMiniProgramService;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
@@ -49,6 +52,22 @@ public class WeChatMiniProgramProperties {
 	 * 若要自定义用户的权限，请开发者自己实现 {@link WeChatMiniProgramService}
 	 */
 	private String defaultRole;
+
+	/**
+	 * 默认 AppID
+	 */
+	@Getter(AccessLevel.NONE)
+	private String defaultAppid;
+
+	public String getDefaultAppid() {
+		if (StringUtils.hasText(defaultAppid)) {
+			return defaultAppid;
+		}
+		if (list.size() > 0) {
+			return list.get(0).appid;
+		}
+		return null;
+	}
 
 	/**
 	 * 微信小程序属性配置类
